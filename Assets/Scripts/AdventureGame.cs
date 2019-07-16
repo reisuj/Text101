3035177ddb073c159a851bc4d4830b0c1bb30b8a
reisuj <reisuj@gmail.com>
2019-07-15 23:21:58 -0400

    Added AdventureGame script and setting up Text Components

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = ("You wake up in a dark cave with a shaft of light coming through the ceiling that you now recall falling through. \"Great!\", you think to yourself. \"I've really gotten myself in trouble now.\"");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
